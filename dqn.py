6f02cb6448d92769ef1f398d22c646d9e1eab59c
yuhwang421 <59779593+yuhwang421@users.noreply.github.com>
2021-03-17 05:02:54 +0800

    Init

---
+++ dqn.py
@@ -0,0 +1,115 @@
+from collections import deque
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.autograd as autograd
+import math, random
+USE_CUDA = torch.cuda.is_available()
+Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
+
+class QLearner(nn.Module):
+    def __init__(self, env, num_frames, batch_size, gamma, replay_buffer):
+        super(QLearner, self).__init__()
+
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.num_frames = num_frames
+        self.replay_buffer = replay_buffer
+        self.env = env
+        # input_shape (1, 84, 84), num_actions 6
+        self.input_shape = self.env.observation_space.shape
+        self.num_actions = self.env.action_space.n
+
+        # mapping function, state to action
+        self.features = nn.Sequential(
+            nn.Conv2d(self.input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+        
+        self.fc = nn.Sequential(
+            nn.Linear(self.feature_size(), 512),
+            nn.ReLU(),
+            nn.Linear(512, self.num_actions)  # what action? move padel left or right
+        )
+        
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+    
+    def feature_size(self):
+        return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
+    
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            state = Variable(torch.FloatTensor(np.float32(state)).unsqueeze(0), requires_grad=True)
+            # TODO: Given state, you should write code to get the Q value and chosen action
+
+            # automatically call the forward function
+            action = torch.argmax(self(state))
+
+            # explore vs exploit
+        else:
+            action = random.randrange(self.env.action_space.n)
+        return action
+
+    def copy_from(self, target):
+        self.load_state_dict(target.state_dict())
+
+        
+def compute_td_loss(model, target_model, batch_size, gamma, replay_buffer):
+    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+
+    state = Variable(torch.FloatTensor(np.float32(state)))
+    next_state = Variable(torch.FloatTensor(np.float32(next_state)).squeeze(1), requires_grad=True)
+    action = Variable(torch.LongTensor(action))
+    reward = Variable(torch.FloatTensor(reward))
+    done = Variable(torch.FloatTensor(done))
+
+    # implement the loss function here
+
+    q_value = model(state).detach().numpy()
+    next_q = model(next_state).detach().numpy()
+    target_q = target_model(next_state).detach().numpy()
+
+    q_value = q_value.gather(1, action.unsqueeze(1)).squeeze(1)
+    next_q = target_q.gather(1, torch.max(next_q, 1)[1].unsqueeze(1)).squeeze(1)
+
+    if done[0]:
+        # for terminal
+        y = reward
+    else:
+        # for non - terminal
+        y = reward + gamma * next_q
+
+    loss = (autograd.Variable(y.data) - q_value).pow(2)
+    return loss
+
+
+class ReplayBuffer(object):
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, done):
+        state = np.expand_dims(state, 0)
+        next_state = np.expand_dims(next_state, 0)
+
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        # TODO: Randomly sampling data with specific batch size from the buffer
+        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+
+        state = np.concatenate(state)
+        next_state = np.concatenate(next_state)
+
+        return state, action, reward, next_state, done
+
+    def __len__(self):
+        return len(self.buffer)
---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+li = [((1,1,8), 175), ((1,1,8), 185), ((1,1,8), 168)]
+
+a, b = zip(*li)
+#a = np.concatenate(a)
+print(a)
+print(b)
